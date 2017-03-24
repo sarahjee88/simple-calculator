b4a41fc7a7d77c56f0d48d58c7e70a75db290b25
Sarah Jee <sarahjee@Sarahui-MacBook-Pro.local>
2017-03-24 19:14:58 +0900

    Fixed few errors

--- simpleCalculator.py
+++ simpleCalculator.py
@@ -1,42 +1,41 @@
-
-
-print "Thanks for using my trashy calculator..." # print text
+print "Thanks for using my trashy calculator... :)" # print text
 print "Please choose an option below:" # print text
 print """
 \n1. Addition
 \n2. Subtraction
 \n3. Multiplication
 \n4. Division
+
+\n\n *NOTICE*
+\nIf you try other number than these four,
+\nyou will get an error message at the end...
 """ # print multiple lines of text
 
 selection = int(input("Choice: "))
 
+
+def addition (num1, num2): # make a function that adds two numbers
+    return num1 + num2
+
+def subtraction (num1, num2): # make a function that subtracts two numbers
+    return num1 - num2
+
+def multiplication (num1, num2): # make a function that multiplies two numbers
+    return num1 * num2
+
+def division (num1, num2): # make a function that divides numbers
+    return num1 / num2
+
 num1 = int(input("Put first number: "))
 num2 = int(input("Put second number: "))
 
-def addition (): # make a function that adds two numbers
-    numAdd = num1 + num2
-    return numAdd
-
-def subtraction (): # make a function that subtracts two numbers
-    numSub = num1 - num2
-    return numSub
-
-def multiplication (): # make a function that multiplies two numbers
-    numMult = num1 * num2
-    return numMult
-
-def division (): # make a function that divides numbers
-    numDiv = num1 / num2
-    return numDiv
-
 if selection == 1:
-    addition()
+    print "%d + %d = " % (num1, num2), addition(num1, num2)
 elif selection == 2:
-    subtraction()
+    print "%d - %d = " % (num1, num2), subtraction(num1, num2)
 elif selection == 3:
-    multiplication()
+    print "%d * %d = " % (num1, num2), multiplication(num1, num2)
 elif selection == 4:
-    division()
+    print "%d / %d = " % (num1, num2), division(num1, num2)
 else:
     print "Invalid input. Please pick another one."
