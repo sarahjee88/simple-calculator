92456cbe013ae3f7654681efdfd0a8b7a856309e
Sarah Jee <sarahjee@Sarahui-MBP.lan>
2017-04-17 10:56:45 +0900

    Fixed few errors

--- simpleCalculator.py
+++ simpleCalculator.py
@@ -26,16 +26,25 @@ def multiplication (num1, num2): # make a function that multiplies two numbers
 def division (num1, num2): # make a function that divides numbers
     return num1 / num2 # return the final value
 
-num1 = int(input("Put first number: ")) # make a variable called num1 that is equal to inputted integer
-num2 = int(input("Put second number: ")) # make a variable called num2 that is equal to inputted integer
-
 if selection == 1: # if statement that works when the selection is equal to 1
+    num1 = int(input("Put first number: ")) # make a variable called num1 that is equal to inputted integer
+    num2 = int(input("Put second number: ")) # make a variable called num2 that is equal to inputted integer
     print "%d + %d = " % (num1, num2), addition(num1, num2) # print formatters and operands with final value
+
 elif selection == 2: # if statement that works when the selection is equal to 2
+    num1 = int(input("Put first number: ")) # make a variable called num1 that is equal to inputted integer
+    num2 = int(input("Put second number: ")) # make a variable called num2 that is equal to inputted integer
     print "%d - %d = " % (num1, num2), subtraction(num1, num2) # print formatters and operands with final value
+
 elif selection == 3: # if statement that works when the selection is equal to 3
+    num1 = int(input("Put first number: ")) # make a variable called num1 that is equal to inputted integer
+    num2 = int(input("Put second number: ")) # make a variable called num2 that is equal to inputted integer
     print "%d * %d = " % (num1, num2), multiplication(num1, num2) # print formatters and operands with final value
+
 elif selection == 4: # if statement that works when the selection is equal to 4
+    num1 = int(input("Put first number: ")) # make a variable called num1 that is equal to inputted integer
+    num2 = int(input("Put second number: ")) # make a variable called num2 that is equal to inputted integer
     print "%d / %d = " % (num1, num2), division(num1, num2) # print formatters and operands with final value
+
 else: # if selection is other than 1, 2, 3, or 4, then operate below lines of code
     print "Invalid input. Please pick another one." # print text
